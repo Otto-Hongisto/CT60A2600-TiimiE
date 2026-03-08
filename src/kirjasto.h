74a03741138e414583c429bc5a934121015d593c
Otto-Hongisto <otto.hongisto@student.lut.fi>
2026-03-08 14:54:11 +0200

    Kaksisuuntainen linkitetty lista

--- src/kirjasto.h
+++ src/kirjasto.h
@@ -1,6 +1,23 @@
 #ifndef KIRJASTO_H
 #define KIRJASTO_H
 
+typedef struct nimilista
+{
+    char* nimi;
+    int nimiLkm;
+    struct nimilista *next;
+    struct nimilista *prev;
+} Nimilista;
+
+typedef struct verkkolista
+{
+    char *yliopisto1;
+    char *yliopisto2;
+    int etaisyys;
+    struct verkkolista *next;
+    struct verkkolista *prev;
+} Verkkolista;
+
 int valikko();
 
 #endif // KIRJASTO_H
